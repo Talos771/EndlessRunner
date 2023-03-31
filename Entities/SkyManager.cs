716d6253f77d0f0c60d2282e2afcde771f22bf3f
Talos771 <114523063+Talos771@users.noreply.github.com>
2023-03-31 18:12:31 +0100

    Update SkyManager.cs

--- Entities/SkyManager.cs
+++ Entities/SkyManager.cs
@@ -1,4 +1,4 @@
-﻿using EndlessRunner.Menu;
+using EndlessRunner.Menu;
 using EndlessRunner.StaticClasses;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -13,28 +13,39 @@ namespace EndlessRunner.Entities
     public class SkyManager : IGameEntity
     {
         private const int STAR_SPAWN_RADIUS = 700;
-        private const int STAR_SPAWN_MIN_DISTANCE = 275;
+        private const int STAR_SPAWN_MIN_DISTANCE = 250;
+
+        private const int MIN_PLANET_POS_Y = 50;
+        private const int MAX_PLANET_POS_Y = 450;
+        private const int MAX_PLANET_DISTANCE = 675;
+        private const int MIN_PLANET_DISTANCE = 250;
+        private double _targetPlanetSpawnScore = 0;
+
+        private double _previousSpawnScore;
 
         private Texture2D _spriteSheetTexture;
 
         private EntityManager _entityManager;
         private MenuManager _menuManager;
         private Astronaut _player;
+        private ScoreBoard _scoreBoard;
 
         public int DrawOrder => 0;
 
-        public SkyManager(Texture2D texture, MenuManager menuManager, EntityManager entityManager, Astronaut player)
+        public SkyManager(Texture2D texture, MenuManager menuManager, EntityManager entityManager, Astronaut player, ScoreBoard scoreBoard)
         {
             _spriteSheetTexture = texture;
             _entityManager = entityManager;
             _menuManager = menuManager;
             _player = player;
+            _scoreBoard = scoreBoard;
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
         }
 
+        //TODO make it so only generates when the game is played
         public void Update(GameTime gameTime)
         {
             List<SkyObject> objects = new List<SkyObject>();
@@ -52,9 +63,19 @@ namespace EndlessRunner.Entities
                 }
             }
 
-            if (!_entityManager.GetEntitiesOfType<SkyPlanet>().Any())
+            // Spawns new planets if the target score for spawning has been reached
+            if (_scoreBoard.Score - _previousSpawnScore >= _targetPlanetSpawnScore)
             {
-                // TODO add logic that has only 1 planet visible at a time
+                Random r = new Random();
+                Vector2 position = new Vector2(DeadSpaceGame.WINDOW_WIDTH, r.Next(MIN_PLANET_POS_Y, MAX_PLANET_POS_Y));
+
+                SkyPlanet p = new SkyPlanet(_spriteSheetTexture, _menuManager, _player, position);
+                _targetPlanetSpawnScore = _scoreBoard.Score + r.NextDouble() * MAX_PLANET_DISTANCE;
+
+                _entityManager.AddEntity(p);
+
+                _targetPlanetSpawnScore = r.NextDouble() * (MAX_PLANET_DISTANCE - MIN_PLANET_DISTANCE) + MIN_PLANET_DISTANCE;
+                _previousSpawnScore = _scoreBoard.Score;
             }
 
             // Adds all the new entities to the entity manager
@@ -66,7 +87,7 @@ namespace EndlessRunner.Entities
             // Removes every object that has moved off the screen
             foreach (SkyObject o in _entityManager.GetEntitiesOfType<SkyObject>())
             {
-                if (o.Position.X < -40)
+                if (o.Position.X < -125)
                     _entityManager.RemoveEntity(o);
             }
         }
@@ -80,6 +101,9 @@ namespace EndlessRunner.Entities
             {
                 _entityManager.RemoveEntity(o);
             }
+
+            _targetPlanetSpawnScore = 0;
+            _previousSpawnScore = 0;
         }
     }
 }

7cb193d5115f43445874a5129ae3bca4d190ae92
Talos771 <114523063+Talos771@users.noreply.github.com>
2023-03-31 17:57:50 +0100

    Update Button.cs

--- Menu/Button.cs
+++ Menu/Button.cs
@@ -1,4 +1,4 @@
-﻿using EndlessRunner.Entities;
+using EndlessRunner.Entities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -6,15 +6,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Windows.Forms;
 using EndlessRunner.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Xml;
+using MonoGame.Extended;
 
 namespace EndlessRunner.Menu
 {
     public class Button : IGameEntity
     {
         MenuManager _menuManager;
+
+        SpriteFont _font;
 
         public string Name { get; set; }
         public Vector2 Position { get; set; }
@@ -25,12 +28,13 @@ namespace EndlessRunner.Menu
         public bool IsPressed { get; set; }
         public Sprite Sprite { get; }
         public int DrawOrder { get; }
-
         private Rectangle ButtonBounds { get; set; }
 
-        public Button(Texture2D spriteSheet, MenuManager menuManager, string name, MenuState displayState, MenuState destination, Vector2 spritePosition, int width, int height, Vector2 position)
+        public Button(Texture2D spriteSheet, MenuManager menuManager, string name, MenuState displayState, MenuState destination, Vector2 spritePosition, int width, int height, Vector2 position, SpriteFont font)
         {
             _menuManager = menuManager;
+
+            _font = font;
 
             Name = name;
             DisplayState = displayState;
@@ -60,6 +64,13 @@ namespace EndlessRunner.Menu
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
             Sprite.Draw(spriteBatch, Position);
+            
+            if (Name == "Change Jump")
+                spriteBatch.DrawString(_font, _menuManager.JumpKey, new Vector2(570 + 27, 150 + 35), Color.White, 0, new Vector2(0, 0), 3, 0, 0);
+            else if (Name == "Change Drop")
+                spriteBatch.DrawString(_font, _menuManager.DropKey, new Vector2(570 + 27, 287 + 35), Color.White, 0, new Vector2(0, 0), 3, 0, 0);
+            else if (Name == "Change Attack")
+                spriteBatch.DrawString(_font, _menuManager.AttackKey, new Vector2(910 + 27, 218 + 35), Color.White, 0, new Vector2(0, 0), 3, 0, 0);
         }
     }
 }
